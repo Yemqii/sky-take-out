76581275adc727f2c1f946ecfff601f6ebfd6532
Yemqii <1030700042@qq.com>
2023-07-20 12:59:04 +0800

    Initial commit

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,107 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import com.sky.vo.DishVO;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ *菜品管理
+ */
+@RestController
+@RequestMapping("/admin/dish")
+@ApiOperation("菜品相关接口")
+@Slf4j
+public class DishController {
+    @Autowired
+    private DishService dishService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @PostMapping
+    @ApiOperation("新增菜品")
+    public Result save(@RequestBody DishDTO dishDTO){
+       log.info("新增菜品：{}",dishDTO);
+       dishService.savewithFlavor(dishDTO);
+
+       //清理缓存数据
+        String key = "dish_"+ dishDTO.getCategoryId();
+        cleanCache(key);
+        return Result.success();
+    }
+
+    //菜品分页查询
+    @GetMapping("/page")
+    @ApiOperation("菜品分页查询")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("菜品分页查询:{}",dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    //删除菜品
+    @DeleteMapping
+    @ApiOperation("菜品批量删除")
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("菜品批量删除:{}",ids);
+        dishService.deleteBatch(ids);
+
+        //将所有菜品缓存数据清理掉
+        cleanCache("dish_*");
+        return Result.success();
+    }
+
+    //根据id查询菜品
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询菜品")
+    public Result<DishVO> getById(@PathVariable Long id){
+        log.info("根据id查询菜品:{}",id);
+        DishVO dishVO = dishService.getByIdWithFlavor(id);
+        return Result.success(dishVO);
+    }
+
+    //修改菜品
+    @PutMapping
+    @ApiOperation("修改菜品")
+    public Result update(@RequestBody DishDTO dishDTO){
+        log.info("修改菜品:{}",dishDTO);
+        dishService.updateWithFlavor(dishDTO);
+
+        //将所有菜品缓存数据清理掉
+        cleanCache("dish_*");
+        return Result.success();
+    }
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("根据分类id查询菜品")
+    public Result<List<Dish>> list(Long categoryId){
+        List<Dish> list = dishService.list(categoryId);
+        return Result.success(list);
+    }
+
+
+    private void cleanCache(String pattern){
+        //将所有菜品缓存数据清理掉
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
+
+
+}
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,46 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
+
+import java.util.List;
+
+/**
+ *
+ */
+public interface DishService {
+    //新增菜品和对应的口味数据
+    public void savewithFlavor(DishDTO dishDTO);
+    //菜品分页查询
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+    //菜品批量删除
+    void deleteBatch(List<Long> ids);
+
+    //根据id查询菜品和对应的口味数据
+    DishVO getByIdWithFlavor(Long id);
+    //根据id修改菜品和对应的口味数据
+    void updateWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    List<Dish> list(Long categoryId);
+
+
+
+
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
+
+
+}
